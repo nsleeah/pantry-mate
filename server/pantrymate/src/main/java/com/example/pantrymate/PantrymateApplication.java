d1cbef4e4b02ad0d1aa3f3efc07cd58c5844b367
Leahnicoles <lnsimonn@hotmail.com>
2023-05-21 00:29:41 +0100

    feat: initial project setup using spring initializr

---
+++ server/pantrymate/src/main/java/com/example/pantrymate/PantrymateApplication.java
@@ -0,0 +1,13 @@
+package com.example.pantrymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantrymateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PantrymateApplication.class, args);
+	}
+
+}
---
+++ server/pantrymate/src/test/java/com/example/pantrymate/PantrymateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pantrymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantrymateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
