58d7a62495e4e861b95b2e4320e9cead55a096e2
Leahnicoles <lnsimonn@hotmail.com>
2023-05-22 11:15:36 +0100

    feat(service): complete inventory service

--- server/pantrymate/src/main/java/com/example/pantrymate/services/InventoryService.java
+++ server/pantrymate/src/main/java/com/example/pantrymate/services/InventoryService.java
@@ -1,4 +1,52 @@
 package com.example.pantrymate.services;
 
+import com.example.pantrymate.models.Inventory;
+import com.example.pantrymate.repositories.FoodBankRepository;
+import com.example.pantrymate.repositories.InventoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class InventoryService {
+    @Autowired
+    private InventoryRepository inventoryRepository;
+    @Autowired
+    private FoodBankRepository foodBankRepository;
+
+    //CRUD
+
+    //CREATE
+    public Inventory addNewInventory(Inventory inventory) {
+        return inventoryRepository.save(inventory);
+    }
+
+    //READ
+    public List<Inventory> getAllInventories() {
+        return inventoryRepository.findAll();
+    }
+
+    public Inventory getInventoryById(Long id) {
+        Optional<Inventory> inventory = inventoryRepository.findById(id);
+        return inventory.orElse(null);
+    }
+
+    //UPDATE
+    public Inventory updateInventory(Inventory inventory) {
+        Optional<Inventory> existingInventory = inventoryRepository.findById(inventory.getId());
+        if (existingInventory.isPresent()){
+            return inventoryRepository.save(inventory);
+        } else {
+            return null;
+        }
+    }
+
+    //DELETE
+    public void deleteInventory(Long id) {
+        inventoryRepository.deleteById(id);
+    }
+    
+
 }
