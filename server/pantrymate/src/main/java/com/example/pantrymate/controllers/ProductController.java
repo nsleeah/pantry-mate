9ef6e717484df1110be92650c937a91615407357
Leahnicoles <lnsimonn@hotmail.com>
2023-05-22 13:14:34 +0100

    feat(controller): add product controller

--- server/pantrymate/src/main/java/com/example/pantrymate/controllers/ProductController.java
+++ server/pantrymate/src/main/java/com/example/pantrymate/controllers/ProductController.java
@@ -1,4 +1,53 @@
 package com.example.pantrymate.controllers;
 
+import com.example.pantrymate.models.Product;
+import com.example.pantrymate.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping()
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping("/addNew")
+    public String addNewProduct(@RequestBody Product product) {
+        Product newProduct = new Product(product.getName(), product.getDescription(), product.getExpiresOn(), product.getCategory(), product.getInventories());
+        productService.addProduct(newProduct);
+        return "Product successfully added!";
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable("id") Long id, @RequestBody Product product) {
+        product.setId(id);
+        Product updatedProduct = productService.updateProduct(product);
+        if (updatedProduct == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok("Product updated!");
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteProduct(@PathVariable("id") Long id) {
+        productService.deleteProduct(id);
+        return "Product successfully deleted!";
+    }
+
+    //Routes Tested via Postman.
+
 }
+
